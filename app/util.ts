f6ae3e4f95740b35f93962c60a092ac33ec78e48
yajium <yajiuma720@gmail.com>
2024-03-16 19:31:20 +0900

    fix: remove unuesd file

--- app/util.ts
+++
@@ -1,8 +0,0 @@
-// import Markdoc from '@markdoc/markdoc';
-
-// export const parseMarkdown = (markdown: string) => {
-//   const ast = Markdoc.parse(markdown);
-//   const content = Markdoc.transform(ast);
-//   const html = Markdoc.renderers.html(content);
-//   return html;
-// };
