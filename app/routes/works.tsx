d1951866eea2ea57888d9fa424f8b92bbb7edbf1
yajium <yajiuma720@gmail.com>
2024-03-29 21:09:59 +0900

    fix(design): works card

--- app/routes/works.tsx
+++ app/routes/works.tsx
@@ -4,7 +4,7 @@ import { H2Centered, WorksCards } from '../components/ui';
 export default createRoute((c) => {
   return c.render(
     <section
-      class='mx-auto grid max-w-md grid-cols-1 gap-20 md:max-w-max'
+      class='grid grid-cols-1 gap-20 px-0 md:px-0 sm:px-14'
       aria-labelledby='works'
     >
       <H2Centered id='works' title='Works' />
