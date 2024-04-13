c515f464466a61686ea7a56ab5d83e4b6e0d8a84
yajium <yajiuma720@gmail.com>
2024-04-13 15:13:15 +0900

    fix: og image and title

--- app/lib/meta.ts
+++ app/lib/meta.ts
@@ -3,6 +3,6 @@ export const favicocnUrl = '/static/favicon.ico';
 export const fontUrl = 'https://images.site.yajihum.dev/sawarabi.woff2';
 
 export const getOgImageUrl = (title: string) => {
-  const url = `https://res.cloudinary.com/dpgixnkg1/image/upload/l_text:noto-snans.ttf_60_bold:${title},co_rgb:156749,w_1000,c_fit,y_-70/v1712987561/ogp_blog.png`;
+  const url = `https://res.cloudinary.com/dpgixnkg1/image/upload/l_text:noto-snans.ttf_60_bold:${title},co_rgb:0A2F21,w_1000,c_fit,y_-70/v1712988671/ogp_blog.png`;
   return url;
 };
