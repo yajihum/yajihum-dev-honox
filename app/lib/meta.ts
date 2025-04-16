3327cbefba8341eee619880b26a03e391629921d
yajium <yajiuma720@gmail.com>
2025-04-16 23:19:55 +0900

    feat: change favicon and og image

--- app/lib/meta.ts
+++ app/lib/meta.ts
@@ -3,6 +3,6 @@ export const favicocnUrl = '/static/favicon.ico';
 export const fontUrl = 'https://images.site.yajihum.dev/sawarabi.woff2';
 
 export const getOgImageUrl = (title: string) => {
-  const url = `https://res.cloudinary.com/dpgixnkg1/image/upload/l_text:noto-snans.ttf_55_bold:${title},co_rgb:0A2F21,w_1000,c_fit,y_-130/v1717843031/og_image.png`;
-  return url;
+  const url = `https://res.cloudinary.com/dpgixnkg1/image/upload/l_text:noto-snans.ttf_55_bold:${title},co_rgb:0A2F21,w_1000,c_fit,y_-130/v1744812926/yajihum_dev_og_image.png`;
+  https: return url;
 };
